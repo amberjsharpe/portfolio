596b50792ebd510bef588be9c7c7f7d5417c4e70
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-03 17:01:57 -0500

    futzed with spacing and pixel pushing

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -17,12 +17,12 @@ class Nav extends Component {
             <Router>
                 <div>
                     <nav className="d-flex justify-content-between row">
-                        <div className="col-12 col-md-3">
+                        <div className="col-12 col-md-9">
                             <Link to='/'>
                                 <div className="logo"><h1>a/#</h1></div>
                             </Link>
                         </div>
-                        <div className="nav-bar d-flex justify-content-between col-12 col-md-9">
+                        <div className="nav-bar d-flex justify-content-between col-12 col-md-3">
                             <div className="nav-item nav-underline"><h2><Link to='/'>HOME</Link></h2></div>
                             <div className="nav-item nav-underline"><h2><Link to='/work'>WORK</Link></h2></div>
                             <div className="nav-item nav-underline"><h2><Link to='/about'>ABOUT</Link></h2></div>
