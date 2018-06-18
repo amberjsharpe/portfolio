1eca62e81a7df15e31f6e947f9d6c2d175f3a08b
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-18 12:00:08 -0500

    image overlay for project links:

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Nav from './Components/Nav';
 import Social from './Components/Social';
+import Work from './Components/Work';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
           <Nav />
           <Social />
         </header>
+        <Work />
       </div>
     );
   }
--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import bookworm from '../images/bookworm.png';
+import smalltalk from '../images/smalltalk.png';
+import './Work.css';
 
 class Work extends Component {
+
     render() {
         return(
-            <div></div>
+            <div className="project-container d-flex justify-content-around align-items-center w-100 mt-5">
+                <div className="project image-overlay-red"></div>
+                <div className="project image-overlay-orange"></div> 
+                <div className="project image-overlay-blue"></div>
+            </div>
         )
     }
 }
