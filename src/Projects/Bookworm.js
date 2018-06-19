a71a6aa401036e73ddbc6fe531aad8ec291a5c79
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-19 09:58:40 -0500

    some changes

--- src/Projects/Bookworm.js
+++ src/Projects/Bookworm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Workbtns from '../Components/Workbtns';
 import parnassus from '../images/parnassus.png';
 import insights from '../images/bookworm-insights.png';
+import persona from '../images/bookworm-persona.png';
 
 class Bookworm extends Component {
     render() {
@@ -21,14 +22,22 @@ class Bookworm extends Component {
                         <p>I began my project by ambitiously deciding to re-design the entire parnassus website. I created a survey to try to get more information on "avid readers" and drew up my initial wireframes. It was during the wireframe process that I realized my time constraint was going to get the best of me. I was going to need to pivot ...</p>
                     </div>
                 </div>
-                <div className="d-flex pt-5">
+                <div className="d-flex pt-5 bottom-border">
                     <div className="w-50">
                         <h3>Research</h3>
                         <p>I sent out a survey asking "avid readers" (those who read more than 20 books a year) to respond to a few questions. The responses that I received showed some clear themes about these "avid readers", so I decided to make an application based on what I discovered during this research. I followed up with several people to do more in-depth interviews about their reading habits and how they interacted with technology during that process.</p>
                     </div>    
                     <img className="insights-img w-50" src={insights} alt="insights" />
                 </div>
-                
+                    <div className="d-flex pt-5">
+                        <div>
+                            <img className="persona-img" src={persona} alt="persona"/>
+                        </div>
+                        <div>
+                            <h3>Persona</h3>
+                            <p>My research led me to develop me to develop this persona. In my design I tried to keep the design fun and based on interfaces that she already uses, like Pinterest and Instagram.</p>
+                        </div>
+                    </div>  
             </div>
         )
     }
