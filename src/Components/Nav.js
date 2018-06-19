b669a7df63d6c780fcb61593249e502509252f21
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-18 21:09:58 -0500

    hi

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,7 +7,7 @@ import {
 } from 'react-router-dom';
 import Introduction from './Introduction';
 import Work from './Work';
-import Play from './Play';
+// import Play from './Play';
 import About from './About';
 import Bookworm from '../Projects/Bookworm';
 import Do from '../Projects/Do';
@@ -28,7 +28,7 @@ class Nav extends Component {
                     <div className="nav d-flex justify-content-between">
                         <h2><NavLink activeClassName='is-active' to='/home'>HOME</NavLink></h2>
                         <h2><NavLink activeClassName='is-active' to='/work'>WORK</NavLink></h2>
-                        <h2><NavLink activeClassName='is-active' to='/play'>PLAY</NavLink></h2>
+                        {/* <h2><NavLink activeClassName='is-active' to='/play'>PLAY</NavLink></h2> */}
                         <h2><NavLink activeClassName='is-active' to='/about'>ABOUT</NavLink></h2>
                     </div>
                 </nav>
@@ -38,7 +38,7 @@ class Nav extends Component {
                     <Route exact path="/work/bookworm" component={Bookworm}/>
                     <Route exact path="/work/do" component={Do}/>
                     <Route exact path="/work/smalltalk" component={Smalltalk}/>
-                    <Route exact path='/play' component={Play} />
+                    {/* <Route exact path='/play' component={Play} /> */}
                     <Route exact path='/about' component={About} />
                 </div>
             </div>
