e58df95cc4cebaeeafdd5e046c3d9cf076931657
Amber Sharpe <amberjsharpe@gmail.com>
2018-06-19 13:11:12 -0500

    bookworm version 2

--- src/Projects/Bookworm.js
+++ src/Projects/Bookworm.js
@@ -8,6 +8,8 @@ import screensketch2 from '../images/ScreenSketch2.jpg';
 import screensketch3 from '../images/ScreenSketch3.jpg';
 import bookwormscreen2 from '../images/bookworm2.png';
 import bookwormscreen3 from '../images/bookworm3.png';
+import modal from '../images/bookworm-modal.png';
+import present from '../images/bookworm-present.jpg';
 
 
 class Bookworm extends Component {
@@ -47,24 +49,54 @@ class Bookworm extends Component {
                     </div>
                     <div className="d-flex">
                         <div className="pt-5 pb-5 bottom-border">
-                            <h3 className="orange-underline">Challenge Statement</h3>
+                            <h3 className="orange-underline pt-1 pb-1">Challenge Statement</h3>
                             <h4>How might I enable my persona to browse books in a relaxed setting without the pressure to purchase, while giving them the ability to track books that they’ve read or that they want to read?</h4>
+                            <p>My solution for this challenge statement came in the form of an application primarily designed to search for books that you would like to read, or that allowed you to track books that you had already read. This met my persona's challenge of wanting to browse for books that she wanted to read without the pressure to buy (and also was achievable within the scope of my technical abilities).</p>
                         </div>
                     </div>
                     <div className="pt-5 pb-5 bottom-border">
                         <h3 className="pt-5 pb-5 orange-underline">Screen Sketches</h3>
+                        <p>I started sketching out what I wanted the application to look like. I wanted a prominent search bar, since that is the main functionality of the site.</p>
                         <div className="d-flex justify-content-around">
                             <img className="screensketch-img" src={screensketch1} alt="screen sketch" />
                             <img className="screensketch-img" src={screensketch2} alt="screen sketch" />
                             <img className="screensketch-img" src={screensketch3} alt="screen sketch" />
                         </div>
                     </div>
-                    <div>
+                    <div className="bottom-border pb-5">
                         <h3 className="pt-5 pb-5 orange-underline">Bookworm v.1</h3>
-                        <div className="d-flex justify-content-around">
+                        <p>This was the final result that I presented from my midstone. I learned from my user interviews that when my persona shops for books she is primary interested in knowing the title, author, and pertinent details about the book. To keep the book cards from looking too cluttered, I programmed a modal to pop up with book details when you click on the book image.</p>
+                        <div className="d-flex justify-content-around flex-wrap">
                             <img className="screengrab-img" src={bookwormscreen2} alt="bookworm search results"/>
                             <img className="screengrab-img" src={bookwormscreen3} alt="bookworm read books page"/>
+                            <img className="screengrab-img pt-5" src={modal} alt="bookworm read books page"/>
                         </div>   
+                    </div>
+                    <div>
+                        <div className="bottom-border pb-5">
+                            <h3 className="pt-5 pb-5 orange-underline">Presentation Time!</h3>
+                            <div className="d-flex">
+                                <img src={present} alt="me presenting my project" />
+                                <p className="pr-5 pl-5">If you can't tell, I had so much fun showing everyone what I had built. Along with a demonstration of my app, I was also able to share some of the lessons that I had learned along the way, and what I would build in version 2.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                    <h3 className="pt-5 pb-5 orange-underline">Version 2</h3>
+                    <p><strong>Now that I've learned React and whole lot more about designing and developing a website, I would approach version two of Bookworm very differently.</strong></p>
+                    <ul>
+                        <li>
+                            <p>I did most of my testing on the final project, which was much harder to change than a prototype would have been. Now prototyping is a crucial part of my design process.</p>
+                        </li>
+                        <li>
+                            <p>I wrote this application in Vanilla Javascript, before I learned a front end framework. The site is not very mobile friendly. In version two, I would re-write the application in React and use a mobile-first approach.</p>
+
+                        </li>
+                        <li>
+                            <p>The biggest lesson that I learned was to not be afraid to pivot. Many times during the project I would be heading down one path, only to hear from my users that they wanted something different. I learned ot keep my process fluid enough that changes can be made along the way.</p>
+                        </li>
+                    </ul>
+
                     </div>
             </div>
         )
